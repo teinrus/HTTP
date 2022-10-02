b749de3f65787f33338f8539fb4d988ebe1b0039
Evgeniy Novichenko <tnrkip@yandex.ru>
2022-10-02 15:34:55 +0300

    added HTTP and create class Cat

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static final String REMOTE_SERVICE_URI = "https://raw.githubusercontent.com/netology-code/" +
+            "jd-homeworks/master/http/task1/cats";
+    public static ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException {
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create().setUserAgent("My Test Service")
+                .setDefaultRequestConfig(RequestConfig.custom().setConnectTimeout(5000)
+
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+
+        HttpGet request = new HttpGet(REMOTE_SERVICE_URI);
+        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+
+        CloseableHttpResponse response = httpClient.execute(request);
+        Arrays.stream(response.getAllHeaders()).forEach(System.out::println);
+
+        List<Cat> posts = mapper.readValue
+                (response.getEntity()
+                        .getContent(), new TypeReference<>() {
+                });
+        posts.stream()
+                .filter(w -> w.getUpvotes() == null)
+                .forEach(s -> System.out.println(s));
+
+    }
+}
+
+
